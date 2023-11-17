7a2b5fe2d89f4a2b8c07ecf05f24ccefaa450da7
sleon204 <sleon87@gmail.com>
2023-11-17 02:39:14 -0800

    Add directory and files for Chapter 06 Classes

---
+++ Chapter 06 Classes/build/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+/*
+------------------------
+   Terminology
+------------------------
+
+TypeScript: A strongly typed programming language compiling to JavaScript, providing better tooling at any scale.
+
+Compiler: Translates TypeScript code (with type annotations) into standard JavaScript for execution.
+
+Static Typing: Specifies types of variables, parameters, and return values at compile-time to catch errors early.
+
+Variable: Named storage holding a value. In TypeScript, allows declaration with specific types for clarity.
+
+Parameter: Variable in function/method declaration representing an input value. Defines required information for function execution.
+
+Expression: Piece of code producing a single value, composed of literals, variables, operators, and function calls.
+
+Functions: Blocks of reusable code performing specific tasks, taking input parameters and optionally returning a value.
+
+Basic Types: string, number, boolean, null, undefined, object, any, union.
+
+Inference: Compiler automatically deduces types of variables and expressions based on context, without explicit type annotations.
+
+Implicit: TypeScript makes decisions/actions without explicit instructions, introducing risks if not used carefully for type safety.
+
+Explicit: Clear, specific actions or declarations leaving no room for assumptions or interpretation.
+
+Type Coercion: Automatic conversion of one data type to another in programming languages.
+
+Concatenation: Process of combining two or more strings into a single string.
+
+RegExp: Powerful tool for pattern matching and text manipulation using search patterns.
+
+Element: Individual item within an array or collection.
+
+Tuple: Ordered collection of elements, each with a specific type, more strict than arrays.
+
+Type Alias: Creating a new name for a type, allowing descriptive or concise naming or complex type creation.
+
+Interface: Contract for an object's shape, specifying names, types, and methods an object must have.
+
+Optional Properties: Properties marked with '?' in interfaces/types, indicating non-mandatory attributes.
+
+Enum: Collection of named constants for related values, facilitating structured usage within a context.
+
+Literal Types: Represent exact values rather than a range, allowing specification of specific values.
+
+Void Type: Denotes absence of any specific type, used for functions returning nothing or undefined.
+
+Signature: Declaration of a function/method including its name, parameters, return type, and visibility/access level.
+
+Rest Parameter: Feature in JavaScript allowing functions to accept an indefinite number of arguments as an array.
+
+Never Type: Type representing values that never occur, used in scenarios where something should never happen or a function never returns.
+
+Type Assertions: Mechanism to tell TypeScript about the intended type of a value, providing flexibility when TypeScript cannot infer it automatically.
+
+Unknown Type: Represents a type-safe 'any'. Unlike any, you can't operate on its values without narrowing the type. Useful for stricter type checking in uncertain scenarios.
+
+
+------------------------
+   Code
+------------------------
+*/
